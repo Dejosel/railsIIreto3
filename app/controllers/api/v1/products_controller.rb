a0751dd87701fb606055e3de33571cf47f737443
Dejosel <newjos7@gmail.com>
2018-06-10 13:44:14 -0500

    Fix route

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,43 +1,39 @@
-module Api
-  module V1
-    class ProductsController < ApplicationController
-      protect_from_forgery with: :null_session
-      def index
-        @products = Product.all
-      end
+class Api::V1::ProductsController < ApplicationController
+  protect_from_forgery with: :null_session
+  def index
+    @products = Product.all
+  end
 
-      def new
-        @product = Product.new
-      end
+  def new
+    @product = Product.new
+  end
 
-      def create
-        @product = Product.create(product_params)
-      end
+  def create
+    @product = Product.create(product_params)
+  end
 
-      def edit
-        @product = Product.find(params[:id])
-      end
+  def edit
+    @product = Product.find(params[:id])
+  end
 
-      def update
-        product = Product.find(params[:id])
-        if product.update(product_params)
-          render json: product
-        else
-          render json: { errors: product.errors }, status: 422
-        end
-      end
-      ​
-      def destroy
-        product = Product.find(params[:id])
-        product.destroy
-        ​
-        head :no_content
-      end
-
-      private
-      def product_params
-        params.require(:product).permit(:name, :price)
-      end
+  def update
+    product = Product.find(params[:id])
+    if product.update(product_params)
+      render json: product
+    else
+      render json: { errors: product.errors }, status: 422
     end
+  end
+  ​
+  def destroy
+    product = Product.find(params[:id])
+    product.destroy
+    ​
+    head :no_content, status: 204
+  end
+
+  private
+  def product_params
+    params.require(:product).permit(:name, :price)
   end
 end
